07f1cb34a0f368d6b506add75a44b852d536dabf
Bradley Nishida <bnishida414@gmail.com>
2020-07-09 17:27:00 -0700

    ?

--- challenge/challenge.go
+++ challenge/challenge.go
@@ -4,8 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	//"strconv"
+	"strconv"
 )
+
+// Important notes:
+// 1. No output until all input has been received
+// 2. No for statements
+// 3. Negative integers are ignored
+
+// To handle note 1: here is our slice to store
+// our values before sending to output
+var sumSquares []int
 
 func main() {
 	// Initialize the scanner
@@ -14,18 +23,33 @@ func main() {
 	scanner.Scan()
 
 	// N is the number of test cases to follow
-	N := scanner.Text()
+	N, _ := strconv.Atoi(scanner.Text())
 	fmt.Println("Test cases:", N)
 
 	// We don't have access to for statements,
 	// so we'll have to use recursion.
-
+	testCaseLoop(N)
 }
 
-func testCaseLoop() {
+// Loop through each test case
+func testCaseLoop(N int) {
+	// Check base case
+	if N <= 0 {
+		return
+	}
+	// Save the sum of squares for this test case
 
+	// Recursion!
+	testCaseLoop(N - 1)
 }
 
-func integerLoop() {
-
+// Loop through each integer and return its square
+func integerLoop(X int) {
+	// Check base case
+	if X == 0 {
+		return
+	}
+	if 
+	// Recursion!
+	integerLoop(X - 1)
 }
